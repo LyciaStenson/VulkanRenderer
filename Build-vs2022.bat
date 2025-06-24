e9b4611b7bf7f70d9afc07218d1356fde7f884b2
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-06-24 17:07:40 +0100

    Moving assets according to relevant project

---
+++ Build-vs2022.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Building Visual Studio 2022 project files.
+
+premake5.exe vs2022
+
+if %errorlevel% neq 0
+(
+	echo Failed to build Visual Studio 2022 project files.
+	exit /b %errorlevel%
+)
+
+echo Visual Studio 2022 project files built successfully.
---
+++ Editor/Source/Private/Main.cpp
@@ -0,0 +1,7 @@
+#include <Core/Engine.h>
+
+int main(int argc, char** argv)
+{
+	VulkanRenderer::Engine engine;
+	engine.Run();
+}
