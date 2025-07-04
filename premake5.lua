e89bee230b0cb4231078e5c25e358f3d9d560d2b
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-07-04 19:10:39 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -22,7 +22,7 @@ workspace "VulkanRenderer"
 group "Dependencies"
 	include "Engine/Vendor/glfw"
 	include "Engine/Vendor/fastgltf"
-	include "Engine/vendor/imgui"
+	include "Editor/vendor/imgui"
 group ""
 
 group "VulkanRenderer"
